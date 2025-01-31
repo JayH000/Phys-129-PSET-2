fe9940cdececa668a77a349987ad29d31459745f
JayH000 <jaylen_holguin@ucsb.edu>
2025-01-30 16:41:24 -0800

    NEW FILE P1.py

---
+++ P1.py
@@ -0,0 +1,15 @@
+import numpy as np
+def spherical_to_cartesian(r,theta, phi):
+    x = r * np.sin(theta) * np.cos(phi)
+    y = r * np.sin(theta) * np.sin(phi)
+    z = r * np.cos(theta)
+    return np.array([x, y, z])
+def cartesian_to_spherical(x, y, z):
+    r = np.sqrt(x**2 + y**2 + z**2)
+    theta = np.arccos(z / r)
+    phi = np.arctan2(y, x)
+    return np.array([r, theta, phi])
+
+
+ 
+  
